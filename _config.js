43805c0ee043b3d6054022f994611ead7e48ed98
MrAugu <39545629+MrAugu@users.noreply.github.com>
2020-04-15 20:07:05 +0300

    Adding config

---
+++ _config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "token": "TOKEN", // https://discordapp.com/developers/applications/ID/bot
+  "mongodbUrl": "MONGODB_URL", // Mongodb connection url.
+  "id": "CLIENT_ID", // https://discordapp.com/developers/applications/ID/information
+  "clientSecret": "CLIENT_SECRET", // https://discordapp.com/developers/applications/ID/information
+  "domain": "http://localhost",
+  "port": 8080
+};
+
+/**
+ * !!!
+ * You need to add a redirect url to https://discordapp.com/developers/applications/ID/oauth2.
+ * Format is: domain:port/callback example http://localhost:8080/callback
+ * - Do not include port if the port is 80.
+ * !!!
+ */
